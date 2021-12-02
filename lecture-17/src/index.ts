159b15aceb188d031261b8d7fa3900a55961b939
Ajit Rana <ajit.thakurs210@gmail.com>
2021-12-03 05:12:55 +0530

    lecture-17-d

--- lecture-17/src/index.ts
+++ lecture-17/src/index.ts
@@ -1,13 +1,11 @@
 import { iif, of } from "rxjs";
 import { ajax } from "rxjs/ajax";
 
+// @example 1: Promise, `fetch`
+
 let isAuthenticated = true;
 
-const obs$ = iif(
-  () => isAuthenticated,
-  ajax.getJSON("https://jsonplaceholder.typicode.com/posts"),
-  of("Not authenticated")
-);
+const obs$ = iif(() => isAuthenticated, fetch("https://jsonplaceholder.typicode.com/posts"), of("Not authenticated"));
 
 obs$.subscribe((value) => {
   console.log("1st observer", value);
@@ -18,3 +16,44 @@ isAuthenticated = false;
 obs$.subscribe((value) => {
   console.log("2nd Observer", value);
 });
+
+/********************************/
+
+// @example 2: async function as Promise and JSON result
+
+// let isAuthenticated = true;
+
+// async function getPosts() {
+//   const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+//   return await res.json();
+// }
+
+// const obs$ = iif(() => isAuthenticated, getPosts(), of("Not authenticated"));
+
+// obs$.subscribe((value) => {
+//   console.log("1st observer", value);
+// });
+
+// isAuthenticated = false;
+
+// obs$.subscribe((value) => {
+//   console.log("2nd Observer", value);
+// });
+
+/********************************/
+
+// @example 3: Both true and false result as Promise
+
+// let isAuthenticated = true;
+
+// const obs$ = iif(() => isAuthenticated, fetch("https://jsonplaceholder.typicode.com/posts"), Promise.resolve("Not authenticated"));
+
+// obs$.subscribe((value) => {
+//   console.log("1st observer", value);
+// });
+
+// isAuthenticated = false;
+
+// obs$.subscribe((value) => {
+//   console.log("2nd Observer", value);
+// });
