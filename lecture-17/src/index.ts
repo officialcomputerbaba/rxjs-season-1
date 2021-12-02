310bbe473f10d54f6566287853c44c61a21d243d
Ajit Rana <ajit.thakurs210@gmail.com>
2021-12-03 05:14:26 +0530

    lecture-17-e

--- lecture-17/src/index.ts
+++ lecture-17/src/index.ts
@@ -1,59 +1,14 @@
-import { iif, of } from "rxjs";
+import { iif, fromEvent } from "rxjs";
 import { ajax } from "rxjs/ajax";
 
-// @example 1: Promise, `fetch`
+const select = document.getElementById("fetchType") as HTMLSelectElement;
 
-let isAuthenticated = true;
+const obs$ = iif(
+  () => select?.value === "posts",
+  ajax.getJSON("https://jsonplaceholder.typicode.com/posts"),
+  ajax.getJSON("https://jsonplaceholder.typicode.com/comments")
+);
 
-const obs$ = iif(() => isAuthenticated, fetch("https://jsonplaceholder.typicode.com/posts"), of("Not authenticated"));
-
-obs$.subscribe((value) => {
-  console.log("1st observer", value);
+fromEvent(document.getElementById("fetchBtn") as HTMLButtonElement, "click").subscribe(() => {
+  obs$.subscribe(console.log);
 });
-
-isAuthenticated = false;
-
-obs$.subscribe((value) => {
-  console.log("2nd Observer", value);
-});
-
-/********************************/
-
-// @example 2: async function as Promise and JSON result
-
-// let isAuthenticated = true;
-
-// async function getPosts() {
-//   const res = await fetch("https://jsonplaceholder.typicode.com/posts");
-//   return await res.json();
-// }
-
-// const obs$ = iif(() => isAuthenticated, getPosts(), of("Not authenticated"));
-
-// obs$.subscribe((value) => {
-//   console.log("1st observer", value);
-// });
-
-// isAuthenticated = false;
-
-// obs$.subscribe((value) => {
-//   console.log("2nd Observer", value);
-// });
-
-/********************************/
-
-// @example 3: Both true and false result as Promise
-
-// let isAuthenticated = true;
-
-// const obs$ = iif(() => isAuthenticated, fetch("https://jsonplaceholder.typicode.com/posts"), Promise.resolve("Not authenticated"));
-
-// obs$.subscribe((value) => {
-//   console.log("1st observer", value);
-// });
-
-// isAuthenticated = false;
-
-// obs$.subscribe((value) => {
-//   console.log("2nd Observer", value);
-// });
