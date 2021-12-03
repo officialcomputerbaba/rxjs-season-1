20c4298ad57af9a6ab30180f228a0435d3e41adc
Ajit Rana <ajit.thakurs210@gmail.com>
2021-12-04 02:01:35 +0530

    lecture-18-c

--- lecture-18/src/index.ts
+++ lecture-18/src/index.ts
@@ -1,57 +1,26 @@
-import { defer, fromEvent, of, iif } from "rxjs";
+import { defer, fromEvent, of } from "rxjs";
 import { ajax } from "rxjs/ajax";
 
-// @example 1
-
-let id = 3;
+const select = document.getElementById("fetchType") as HTMLSelectElement;
 
 const obs$ = defer(() => {
-  if (id > 0 && id <= 3) {
-    return ajax.getJSON(`https://jsonplaceholder.typicode.com/users/${id}`);
+  const fetchType = select?.value;
+
+  switch (fetchType) {
+    case "posts":
+      return ajax.getJSON("https://jsonplaceholder.typicode.com/posts");
+
+    case "comments":
+      return ajax.getJSON("https://jsonplaceholder.typicode.com/comments");
+
+    case "users":
+      return ajax.getJSON("https://jsonplaceholder.typicode.com/users");
+
+    default:
+      return of([]);
   }
-
-  return of([]);
 });
 
-fromEvent(document.getElementById("actionBtn") as HTMLButtonElement, "click").subscribe(() => {
-  obs$.subscribe((data) => {
-    id -= 1;
-    console.log(data);
-  });
+fromEvent(document.getElementById("fetchBtn") as HTMLButtonElement, "click").subscribe(() => {
+  obs$.subscribe(console.log);
 });
-
-/********************************/
-
-// @example 2: diff b/w `defer` and `iif`
-
-// let id = 3;
-
-// const obs$ = iif(() => id > 0 && id <= 3, ajax.getJSON(`https://jsonplaceholder.typicode.com/users/${id}`), of([]));
-
-// fromEvent(document.getElementById("actionBtn") as HTMLButtonElement, "click").subscribe(() => {
-//   obs$.subscribe((data) => {
-//     id -= 1;
-//     console.log(data);
-//   });
-// });
-
-/********************************/
-
-// @example 3: Returning Promise in`defer`
-
-// let id = 3;
-
-// const obs$ = defer(() => {
-//   if (id > 0 && id <= 3) {
-//     return fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
-//   }
-
-//   return of([]);
-// });
-
-// fromEvent(document.getElementById("actionBtn") as HTMLButtonElement, "click").subscribe(() => {
-//   obs$.subscribe((data) => {
-//     id -= 1;
-//     console.log(data);
-//   });
-// });
