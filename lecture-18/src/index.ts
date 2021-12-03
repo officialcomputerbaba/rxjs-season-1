780b46eacc912bdd3a3b9868196f57caf72d0867
Ajit Rana <ajit.thakurs210@gmail.com>
2021-12-04 02:05:36 +0530

    lecture-18-d

--- lecture-18/src/index.ts
+++ lecture-18/src/index.ts
@@ -1,26 +1,28 @@
 import { defer, fromEvent, of } from "rxjs";
-import { ajax } from "rxjs/ajax";
 
-const select = document.getElementById("fetchType") as HTMLSelectElement;
+const ball = document.getElementById("ball") as HTMLDivElement;
 
 const obs$ = defer(() => {
-  const fetchType = select?.value;
+  const newBall = {
+    left: ball.getBoundingClientRect().x + 10 + "px",
+    top: ball.getBoundingClientRect().y + 5 + "px",
+    background: "#" + (((1 << 24) * Math.random()) | 0).toString(16),
+    class: "animationBall",
+  };
 
-  switch (fetchType) {
-    case "posts":
-      return ajax.getJSON("https://jsonplaceholder.typicode.com/posts");
-
-    case "comments":
-      return ajax.getJSON("https://jsonplaceholder.typicode.com/comments");
-
-    case "users":
-      return ajax.getJSON("https://jsonplaceholder.typicode.com/users");
-
-    default:
-      return of([]);
-  }
+  return of(newBall);
 });
 
-fromEvent(document.getElementById("fetchBtn") as HTMLButtonElement, "click").subscribe(() => {
-  obs$.subscribe(console.log);
+fromEvent<PointerEvent>(document, "click").subscribe((evt) => {
+  ball.style.left = evt.clientX + "px";
+  ball.style.top = evt.clientY + "px";
+
+  obs$.subscribe((newBall) => {
+    const ballElement = document.createElement("div");
+
+    ballElement.classList.add(newBall.class);
+    Object.assign(ballElement.style, newBall);
+
+    document.body.appendChild(ballElement);
+  });
 });
