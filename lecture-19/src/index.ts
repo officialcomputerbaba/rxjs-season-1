0935c0ac1b26bd5aa021eff7a6d56b8eaa5f91a1
Ajit Rana <ajit.thakurs210@gmail.com>
2022-01-21 03:09:23 +0530

    lecture-19-c

--- lecture-19/src/index.ts
+++ lecture-19/src/index.ts
@@ -1,10 +1,18 @@
 import { of, from, concat } from "rxjs";
+import { ajax } from "rxjs/ajax";
 
 const num$ = of(23, 12, 3);
-const names$ = from(["Ajit", "Amitabh"]);
 
-concat(num$, names$).subscribe({
+// correct url -> https://jsonplaceholder.typicode.com/todos
+const todos$ = ajax.getJSON("https://jsonplaceholder.typicode.com/todoss"); // this will create an error
+
+const names$ = from(["Ajit", "Amitabh"]); // this observable will never be subscribed by `concat`
+
+concat(num$, todos$, names$).subscribe({
   next: console.log,
+  error: (err) => {
+    console.log("Error", err);
+  },
   complete: () => {
     console.log("Completed");
   },
