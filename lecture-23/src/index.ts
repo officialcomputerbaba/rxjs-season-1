03996e35ce4ed1581de09cff7963eab641bae508
Ajit Rana <ajit.thakurs210@gmail.com>
2022-02-03 02:00:00 +0530

    lecture-23-3

--- lecture-23/src/index.ts
+++ lecture-23/src/index.ts
@@ -1,8 +1,22 @@
-import { zip, from } from "rxjs";
+import { zip, concat, combineLatest, from } from "rxjs";
 
 const lang$ = from(["JS", "JAVA", "C"]);
 const frameworks$ = from(["Angular", "Spring"]);
 
-zip(lang$, frameworks$).subscribe((resultList) => {
+console.log("--concat--");
+
+concat(lang$, frameworks$).subscribe((value) => {
+  console.log(value);
+});
+
+console.log("--combineLatest--");
+
+combineLatest([lang$, frameworks$]).subscribe((resultList) => {
   console.log(resultList);
+});
+
+console.log("--zip--");
+
+zip(lang$, frameworks$).subscribe((resultPair) => {
+  console.log(resultPair);
 });
