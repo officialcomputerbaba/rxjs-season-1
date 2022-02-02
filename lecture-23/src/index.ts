955c24186ddcd6b013d71c62ba217017aa58a3c3
Ajit Rana <ajit.thakurs210@gmail.com>
2022-02-03 02:08:05 +0530

    lecture-23-9

--- lecture-23/src/index.ts
+++ lecture-23/src/index.ts
@@ -1,10 +1,16 @@
-import { zip, from } from "rxjs";
+import { zip, from, range } from "rxjs";
+import { errorObservable } from "./custom-operators";
 
 const notebooks$ = from(["Dell", "Mac", "Hp"]);
-const ratings$ = from([5, 4, 1]);
+const ratings$ = range(1, 5);
+const error$ = errorObservable();
 
-zip(notebooks$, ratings$).subscribe({
+zip(notebooks$, ratings$, error$).subscribe({
   next: console.log,
+
+  error: (err) => {
+    console.log("Error", err);
+  },
 
   complete: () => {
     console.log("Completed");
