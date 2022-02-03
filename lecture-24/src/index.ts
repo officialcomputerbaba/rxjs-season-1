82e88d12d71a22761f338d5a83df9a4b475725be
Ajit Rana <ajit.thakurs210@gmail.com>
2022-02-03 21:31:33 +0530

    lecture-24-2

--- lecture-24/src/index.ts
+++ lecture-24/src/index.ts
@@ -1,18 +1,6 @@
-import { race } from "rxjs";
-import { namedInterval } from "./custom-operators";
+import { race, of, interval } from "rxjs";
 
-// @example 1: Finite Observables
+const roles$ = of("Admin", "User", "Tester");
+const interval$ = interval(500);
 
-const timer1$ = namedInterval(1000, "timer1", 4);
-const timer2$ = namedInterval(500, "timer2", 5);
-const timer3$ = namedInterval(1500, "timer3", 3);
-
-race(timer1$, timer2$, timer3$).subscribe(console.log);
-
-// @example 2: Infinite Observables
-
-// const timer1$ = namedInterval(1000, "timer1");
-// const timer2$ = namedInterval(500, "timer2");
-// const timer3$ = namedInterval(1500, "timer3");
-
-// race(timer1$, timer2$, timer3$).subscribe(console.log);
+race(roles$, interval$).subscribe(console.log);
