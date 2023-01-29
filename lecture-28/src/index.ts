90e2d30ea074e8545b6a95795360aa478225b1b5
Ajit <ajit.thakurs210@gmail.com>
2023-01-29 16:31:35 +0530

    lecture-28-5

--- lecture-28/src/index.ts
+++ lecture-28/src/index.ts
@@ -1,12 +1,17 @@
-import { fromEvent } from "rxjs";
+import { ajax } from "rxjs/ajax";
 import { map } from "rxjs/operators";
 
-fromEvent<KeyboardEvent>(document.getElementById("inputBox") as HTMLInputElement, "keyup")
-  .pipe(
-    map((evt) => {
-      const input = evt.target as HTMLInputElement;
+interface User {
+  name: string;
+  id: string;
+  email: string;
+}
 
-      return input.value;
+ajax
+  .getJSON<User[]>("https://jsonplaceholder.typicode.com/users")
+  .pipe(
+    map((users) => {
+      return users.map((user) => user.name);
     })
   )
   .subscribe(console.log);
