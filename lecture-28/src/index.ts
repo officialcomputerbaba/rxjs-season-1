7c36648b1b7db8d9ce85ab84390b56b9a9fcaa4d
Ajit <ajit.thakurs210@gmail.com>
2023-01-29 16:33:43 +0530

    lecture-28-6

--- lecture-28/src/index.ts
+++ lecture-28/src/index.ts
@@ -1,17 +1,10 @@
-import { ajax } from "rxjs/ajax";
+import { from, of } from "rxjs";
 import { map } from "rxjs/operators";
 
-interface User {
-  name: string;
-  id: string;
-  email: string;
-}
-
-ajax
-  .getJSON<User[]>("https://jsonplaceholder.typicode.com/users")
+from(["Ajit", "Ronaldo", "Dr. Strange", "Ashish"])
   .pipe(
-    map((users) => {
-      return users.map((user) => user.name);
+    map((name) => {
+      return of(name.toUpperCase());
     })
   )
   .subscribe(console.log);
