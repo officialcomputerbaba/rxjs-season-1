731e0864862bf90df42fb0604ed50231bea4c9bc
Ajit <ajit.thakurs210@gmail.com>
2023-02-11 23:11:20 +0530

    lecture-30-7

--- lecture-30/src/index.ts
+++ lecture-30/src/index.ts
@@ -1,6 +1,13 @@
 import { from, timer } from "rxjs";
-import { concatMap } from "rxjs/operators";
+import { map, concatMap } from "rxjs/operators";
 
 from([timer(1000), timer(500), timer(2000)])
-  .pipe(concatMap((timer$) => timer$))
-  .subscribe(console.log);
+  .pipe(
+    concatMap((timer$) => timer$),
+    map((v: number, idx: number) => {
+      return idx + 1;
+    })
+  )
+  .subscribe((i: number) => {
+    console.log(`Timer ${i} completed`);
+  });
