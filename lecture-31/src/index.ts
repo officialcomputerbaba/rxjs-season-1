73a2021b2158e43a3a16fdc45ae611825617ef74
Ajit <ajit.thakurs210@gmail.com>
2023-02-12 00:42:08 +0530

    lecture-31-3

--- lecture-31/src/index.ts
+++ lecture-31/src/index.ts
@@ -1,10 +1,17 @@
-import { fromEvent, interval } from "rxjs";
+import { from } from "rxjs";
+import { ajax } from "rxjs/ajax";
 import { mergeMap } from "rxjs/operators";
 
-fromEvent(document, "click")
+const urls = [
+  "https://jsonplaceholder.typicode.com/posts/1",
+  "https://jsonplaceholder.typicode.com/posts/2",
+  "https://jsonplaceholder.typicode.com/posts/3",
+];
+
+from(urls)
   .pipe(
-    mergeMap((pointerEvent: PointerEvent, idx: number) => {
-      return interval(1000);
+    mergeMap((url: string) => {
+      return ajax.getJSON(url);
     })
   )
   .subscribe(console.log);
