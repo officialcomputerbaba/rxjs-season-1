de4bd13f0e2278018c9f0b1ee59343569015990e
Ajit <ajit.thakurs210@gmail.com>
2023-02-11 23:09:42 +0530

    lecture-30-6

--- lecture-30/src/index.ts
+++ lecture-30/src/index.ts
@@ -1,31 +1,6 @@
-import { from } from "rxjs";
+import { from, timer } from "rxjs";
 import { concatMap } from "rxjs/operators";
 
-const urls = [
-  "https://jsonplaceholder.typicode.com/users/1",
-  "https://jsonplaceholder.typicode.com/posts/1",
-  "https://jsonplaceholder.typicode.com/comments/1",
-];
-
-// example 1
-
-from(urls)
-  .pipe(
-    concatMap((url: string) => {
-      return fetch(url).then((res) => res.json());
-    })
-  )
+from([timer(1000), timer(500), timer(2000)])
+  .pipe(concatMap((timer$) => timer$))
   .subscribe(console.log);
-
-// --------------------
-
-// example 2: async function
-
-// from(urls)
-//   .pipe(
-//     concatMap(async (url: string) => {
-//       const res = await fetch(url);
-//       return await res.json();
-//     })
-//   )
-//   .subscribe(console.log);
