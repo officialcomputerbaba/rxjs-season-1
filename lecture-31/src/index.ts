251df4f4aacffff4cb0b0998ec7544a77dca6456
Ajit <ajit.thakurs210@gmail.com>
2023-02-12 00:44:31 +0530

    lecture-31-4

--- lecture-31/src/index.ts
+++ lecture-31/src/index.ts
@@ -6,12 +6,13 @@ const urls = [
   "https://jsonplaceholder.typicode.com/posts/1",
   "https://jsonplaceholder.typicode.com/posts/2",
   "https://jsonplaceholder.typicode.com/posts/3",
+  "https://jsonplaceholder.typicode.com/posts/4",
 ];
 
 from(urls)
   .pipe(
     mergeMap((url: string) => {
       return ajax.getJSON(url);
-    })
+    }, 2) // with concurrent factor 2, `concatMap` always has 1
   )
   .subscribe(console.log);
